12a00363d1b66c4fbbf80b1d43738aff1a47c866
yuqzii <p4r4yuqzii@gmail.com>
2025-05-01 20:03:40 +0200

    refactor: separate command to other file

---
+++ internal/messageCommands.go
@@ -0,0 +1,8 @@
+package messageCommands
+
+import "github.com/bwmarrin/discordgo"
+
+func Hello(s *discordgo.Session, m *discordgo.MessageCreate) (error) {
+	_, err := s.ChannelMessageSend(m.ChannelID, "world!")
+	return err
+}
