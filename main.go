47ebaeba53b14220311846840e4790dbcb55d8e5
yuqzii <p4r4yuqzii@gmail.com>
2025-05-01 16:05:07 +0200

    feat: set up basic bot

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	token := os.Getenv("TOKEN")
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		// Don't react to messages from this bot
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "world!")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close() // Close session when application exits
+
+	log.Println("Bot is online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
