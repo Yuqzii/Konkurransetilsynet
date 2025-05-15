cc396f756856ee14c00a3aab7f5c772af95265ce
Marbjo07 <marius.bjorhei@gmail.com>
2025-05-15 23:35:10 +0200

    refactor: drop saving test cases as json, cleaned testing code

--- internal/guessTheFunction/expr_test.go
+++ internal/guessTheFunction/expr_test.go
@@ -2,104 +2,28 @@ package guessTheFunction
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 	"testing"
 )
 
 func Test_MarshalAndUnMarshal(t *testing.T) {
-	// Defined as code since loading is being tested
-	functionDefinitions := [...]expr{
-		add{
-			variable{}, number{Value: 10}},
-
-		add{
-			variable{}, number{Value: 1}},
-
-		subtract{
-			variable{}, number{Value: 31}},
-
-		subtract{
-			variable{}, number{Value: 9}},
-
-		add{
-			variable{}, add{number{Value: 13}, number{Value: 3}}},
-
-		add{
-			number{Value: 12}, subtract{variable{}, number{Value: 2}},
-		},
-
-		add{
-			subtract{
-				variable{},
-				number{Value: 210},
-			},
-			add{
-				variable{},
-				number{Value: 1},
-			}},
-
-		add{
-			multiply{number{Value: 3}, variable{}},
-			number{Value: 2},
-		},
-
-		subtract{
-			multiply{number{Value: 10}, variable{}},
-			number{Value: 9},
-		},
-
-		add{
-			multiply{number{Value: -10}, variable{}},
-			number{Value: 3},
-		},
-
-		add{
-			multiply{variable{}, number{Value: -5}},
-			number{Value: 10},
-		},
-
-		add{
-			number{Value: -1},
-			multiply{number{Value: 7}, variable{}},
-		},
-
-		number{Value: -1},
-
-		add{
-			number{Value: 1},
-			multiply{number{Value: -1}, variable{}},
-		},
-	}
-
-	for index, functionExpr := range functionDefinitions {
-		t.Run(fmt.Sprintf("function index:%d", index), func(t *testing.T) {
+	for i, correctFunc := range TestCases_SavingLoading {
+		tName := fmt.Sprintf("function index: %d", i)
+		t.Run(tName, func(t *testing.T) {
 			// Serialize to JSON
-			jsonData, err := MarshalExpr(functionExpr)
+			jsonData, err := MarshalExpr(correctFunc)
 			if err != nil {
-				t.Fatal("unexpected error on function index,", index, "error,", err)
+				t.Fatalf("unexpected error on function index, %d error, %s", i, err)
+				return
 			}
 
 			// Deserialize back
-			loadedExpr, err := unmarshalExpr(jsonData)
+			parsedFunc, err := unmarshalExpr(jsonData)
 			if err != nil {
 				panic(err)
 			}
 
-			for i := 0; i < numberSamplesPerFunctionTest; i++ {
-				x := rand.Float64() * 100
-				y_correct := functionExpr.Eval(x)
-				y_parsed := loadedExpr.Eval(x)
-
-				absolute_difference := math.Abs(y_parsed - y_correct)
-				y_average := (y_parsed + y_correct) / 2.0
-
-				relative_difference := absolute_difference / y_average
-
-				if relative_difference > maxTolerableError {
-					t.Logf("failed on test index: %d, x: %f, y: %f, y_reconstruct: %f", index, x, functionExpr.Eval(x), loadedExpr.Eval(x))
-				}
-			}
+			// Validate parsedFunc
+			AssertFunctionsApproxEqual(parsedFunc, correctFunc, t)
 		})
 	}
 }
--- internal/guessTheFunction/guessTheFunction_test.go
+++ internal/guessTheFunction/guessTheFunction_test.go
@@ -1,93 +1,57 @@
 package guessTheFunction
 
 import (
-	"encoding/json"
-	"io/fs"
 	"math"
 	"math/rand"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
-const numberSamplesPerFunctionTest int = 100
+const xValuesLowerBound float64 = -1000
+const xValuesUpperBound float64 = 1000
+const numberSamplesPerFunctionTest uint16 = 100
 const maxTolerableError float64 = 1e-5
 
-func loadAllTestCases(dir string) ([]TestCase, error) {
-	var allTests []TestCase
-
-	// all files in dir
-	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if filepath.Ext(path) == ".json" {
-			data, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
-
-			var tests []TestCase
-			if err := json.Unmarshal(data, &tests); err != nil {
-				return err
-			}
-
-			allTests = append(allTests, tests...)
-		}
-
-		return nil
-	})
-
+func logFunctionDefinition(fn expr, t *testing.T) {
+	data, err := MarshalExpr(fn)
 	if err != nil {
-		return nil, err
+		t.Logf("unpacking error %s", err)
+	} else {
+		t.Logf("function: %s", string(data))
 	}
-	return allTests, nil
+}
+
+func AssertFunctionsApproxEqual(parsedFunc expr, correctFunc expr, t *testing.T) {
+	for range numberSamplesPerFunctionTest {
+		// Sample in interval given
+		x := rand.Float64()*(xValuesUpperBound+math.Abs(xValuesLowerBound)) - xValuesLowerBound
+
+		y_correct := correctFunc.Eval(x)
+		y_parsed := parsedFunc.Eval(x)
+
+		// Find relative difference
+		absolute_difference := math.Abs(y_parsed - y_correct)
+		y_average := (y_parsed + y_correct) / 2.0
+		relative_difference := absolute_difference / y_average
+
+		if relative_difference > maxTolerableError {
+			logFunctionDefinition(parsedFunc, t)
+			logFunctionDefinition(correctFunc, t)
+
+			t.Fatalf("functions did not produce same value, x: %f y: %f y_pred: %f", x, correctFunc.Eval(x), parsedFunc.Eval(x))
+		}
+	}
 }
 
 func TestMakeNewFunction(t *testing.T) {
-	functionDefinitions, err := loadAllTestCases("testdata")
-	if err != nil {
-		t.Fatalf("could not load test cases: %v", err)
-	}
-
-	for index, testData := range functionDefinitions {
-		t.Run(testData.Input, func(t *testing.T) {
-			parsedFunction, err := makeNewFunction(testData.Input)
+	for i, tc := range TestCases_FunctionParsing {
+		t.Run(tc.Input, func(t *testing.T) {
+			parsedFunc, err := makeNewFunction(tc.Input)
 			if err != nil {
-				t.Fatal("unexpected error on function idx,", index, "error,", err)
+				t.Fatalf("unexpected error on function index, %d error, %s", i, err)
 				return
 			}
 
-			expectedFunction := testData.Expected
-
-			for i := 0; i < numberSamplesPerFunctionTest; i++ {
-				x := rand.Float64() * 100
-				y_correct := expectedFunction.Eval(x)
-				y_parsed := parsedFunction.Eval(x)
-
-				absolute_difference := math.Abs(y_parsed - y_correct)
-				y_average := (y_parsed + y_correct) / 2.0
-
-				relative_difference := absolute_difference / y_average
-
-				if relative_difference > maxTolerableError {
-					data, err := MarshalExpr(parsedFunction)
-					if err != nil {
-						t.Logf("unpacking error %s", err)
-					} else {
-						t.Logf("decoded function: %s", string(data))
-					}
-
-					data, err = MarshalExpr(expectedFunction)
-					if err != nil {
-						t.Logf("unpacking error %s", err)
-					} else {
-						t.Logf("correct function: %s", string(data))
-					}
-					t.Fatalf("failed on test idx %d function, %s x: %f y: %f y_pred: %f", index, testData.Input, x, expectedFunction.Eval(x), parsedFunction.Eval(x))
-				}
-			}
+			AssertFunctionsApproxEqual(parsedFunc, tc.Expected, t)
 		})
 	}
 }
