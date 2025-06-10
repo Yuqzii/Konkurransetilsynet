6ff34b2cbf3d144c8fb72d81e66d39a30c06f6f2
yuqzii <p4r4yuqzii@gmail.com>
2025-06-10 21:22:34 +0200

    feat(cf): add function to get handles in guild

--- internal/codeforces/leaderboard.go
+++ internal/codeforces/leaderboard.go
@@ -6,11 +6,34 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/jackc/pgx/v5"
+	"github.com/yuqzii/konkurransetilsynet/internal/database"
 )
 
 type ratingChange struct {
 	OldRating int `json:"oldRating"`
 	NewRating int `json:"newRating"`
+}
+
+func getCodeforcesInGuild(guildID string, s *discordgo.Session) (result []string, err error) {
+	guild, err := s.State.Guild(guildID)
+	if err != nil {
+		return nil, fmt.Errorf("getting guild from id %s: %w", guildID, err)
+	}
+
+	for _, member := range guild.Members {
+		handle, err := database.GetConnectedCodeforces(member.User.ID)
+		// ErrNoRows expected if the user has not connected their Codeforces
+		if err != nil && err != pgx.ErrNoRows {
+			return nil, fmt.Errorf("getting Codeforces handle of %s (%s): %w",
+				member.User.ID, member.User.Username, err)
+		}
+		result = append(result, handle)
+	}
+
+	return result, nil
 }
 
 func getRating(handle string) (rating *ratingChange, err error) {
