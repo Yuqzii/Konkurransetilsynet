1b438b6bd10b13dcd91a3d4d1a327c41cad7179b
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-01-12 19:54:30 +0100

    feat: make script take compose profile as argument

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Cron job that checks for updates and rebuilds if it finds any.
+# Takes the compose profile as argument (prod or dev).
 
 set -euo pipefail
 
@@ -20,7 +21,7 @@ echo "Changes detected, pulling updates..."
 git pull origin main
 
 echo "Rebuilding containers..."
-make PROFILE=prod DETACHED=-d PULL=--pull
+make PROFILE=$1 DETACHED=-d PULL=--pull
 
 echo "Cleaning up old images..."
 docker image prune -f
